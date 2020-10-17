68a416805e39de4cc34e0f48996208c071cdc840
PatrickCHennessey <patrick.c.hennessey@gmail.com>
2020-10-17 11:33:02 -0400

    Final submit

--- static/radar_scripts.js
+++ static/radar_scripts.js
@@ -20,33 +20,46 @@ function update_radar() {
         Energy = []
         Liveness = []
         Loudness = []
+        Popularity = []
+        Tempo = []
+        Valence = []
         headers = []
         
         data.forEach(function(d){
-            // Add in missing Spotify ranges 
+        //Issue: Needed to add in additional attributes
+        //Issue: Certain genres given 1 value by default obviously misrepresenting actual data values   
             Danceability.push(d.Danceability)
             Duration.push(d.Duration)
             Energy.push(d.Energy)
             Liveness.push(d.Liveness)
             Loudness.push(d.Loudness)
-            
+            Popularity.push(d.Popularity)
+            Tempo.push(d.Tempo)
+            Valence.push(d.Valence)
             headers.push(d.top_genre)
         })
         
-        // Add in missing Spotify ranges
         norm_Danceability = Danceability.map(x => (x-Math.min(...Danceability))/(Math.max(...Danceability)-Math.min(...Danceability)))
         norm_Duration = Duration.map(x => (x-Math.min(...Duration))/(Math.max(...Duration)-Math.min(...Duration)))
         norm_Energy = Energy.map(x => (x-Math.min(...Energy))/(Math.max(...Energy)-Math.min(...Energy)))
         norm_Liveness = Liveness.map(x => (x-Math.min(...Liveness))/(Math.max(...Liveness)-Math.min(...Liveness)))
         norm_Loudness = Loudness.map(x => (x-Math.min(...Loudness))/(Math.max(...Loudness)-Math.min(...Loudness)))
-        
-        // Add in missing Spotify ranges
+        norm_Popularity = Popularity.map(x => (x-Math.min(...Popularity))/(Math.max(...Popularity)-Math.min(...Popularity)))
+        norm_Tempo = Tempo.map(x => (x-Math.min(...Tempo))/(Math.max(...Tempo)-Math.min(...Tempo)))
+        norm_Valence = Valence.map(x => (x-Math.min(...Valence))/(Math.max(...Valence)-Math.min(...Valence)))
+
         Danceability = ['Danceability'].concat(norm_Danceability)
         Duration = ['Duration'].concat(norm_Duration)
         Energy = ['Energy'].concat(norm_Energy)
         Liveness = ['Liveness'].concat(norm_Liveness)
         Loudness = ['Loudness'].concat(norm_Loudness)
+        Popularity = ['Popularity'].concat(norm_Popularity)
+        Tempo = ['Tempo'].concat(norm_Tempo)
+        Valence = ['Valence'].concat(norm_Valence)
         headers = ['#'].concat(headers)
+        
+        //Test to eliminate faulty data representation
+        console.log(Valence)
 
         var chartData = {
             title: 'Top Spotify Songs by Attributes (1999-2019)',
@@ -56,21 +69,28 @@ function update_radar() {
                 Duration,
                 Energy,
                 Liveness,
-                Loudness
-                //Add other Spotify attributes here as well
+                Loudness,
+                Popularity,
+                Tempo,
+                Valence
             ]
         };
         // create radar chart
         var chart = anychart.radar();
 
         // set default series type
-        chart.defaultSeriesType('area');
+        //chart.defaultSeriesType('area');
+        chart
+        .title('Comparison Chart')
+        
+        // set chart legend
+        .legend(true);
 
         // set chart data
         chart.data(chartData);
         
         // force chart to stack values by Y scale.
-        chart.yScale().stackMode('percent');
+        chart.yScale().minimum(0).maximum(1).ticks({ interval:0.25}).stackMode('percent');
         
         // set yAxis settings
         chart.yAxis().stroke('#545f69');
@@ -96,74 +116,4 @@ function update_radar() {
 
 }
 //Event Listener
-d3.select("#update_radar").on("click"), update_radar()
-    
-
-
-    // Left off here 10/15/2020: Problem with JS attempts to decode the reply from MongoDB XXXXXXX
-    // Current Issue: Getting the data from Mongo DB into Javascript
-    // use of filter function on variable year
-
-
-    // Sanhita : Will need to convert Music_Genre.ipynb into a music_genre.py
-    // Sanhita: Will then need to create a route in app.py to call music_genre.py
-    // Sanhita: Conversion will be very similar to "Mission to MArs scrape example" used
-    //              in scrape_mars.py
-
-    // Read response from API
-
-
-    // Update/display the graph
-
-
-
-
-
-//the codes for the viz (AnyChart)
-
-
-// anychart.onDocumentReady(function () {
-//     // create data set on our data
-//     var chartData = {
-//       title: 'Region Sales by Year (2007-2011) (Stacked)',
-//       header: ['#', 'Arizona', 'Florida', 'Nevada'],
-//       rows: [
-//         ['2007', 1368763, 1991297, 431097],
-//         ['2008', 799873, 1254823, 561983],
-//         ['2009', 1497653, 1732987, 1019874],
-//         ['2010', 1351874, 332871, 2027634],
-//         ['2011', 1582987, 649853, 1961085]
-//       ]
-//     };
-  
-//     // create radar chart
-//     var chart = anychart.radar();
-  
-//     // set default series type
-//     chart.defaultSeriesType('area');
-  
-//     // set chart data
-//     chart.data(chartData);
-  
-//     // force chart to stack values by Y scale.
-//     chart.yScale().stackMode('value');
-  
-//     // set yAxis settings
-//     chart.yAxis().stroke('#545f69');
-//     chart.yAxis().ticks().stroke('#545f69');
-  
-//     // set yAxis labels settings
-//     chart
-//       .yAxis()
-//       .labels()
-//       .fontColor('#545f69')
-//       .format('{%Value}{scale:(1000000)|(M)}');
-  
-//     // set chart legend settings
-//     chart.legend().align('center').position('center-bottom').enabled(true);
-  
-//     // set container id for the chart
-//     chart.container('container');
-//     // initiate chart drawing
-//     chart.draw();
-//   });
+d3.select("#update_radar").on("click", function(){update_radar()})
